695eab4680ce7cf8b56df7c026246bb9e4a4baf8
DevAles <devalesoficial@gmail.com>
2023-01-23 19:17:29 -0300

    Read scss files, modify and write

--- src/main.rs
+++ src/main.rs
@@ -25,40 +25,41 @@ fn find_scss_files(path: &Path) -> Vec<String> {
 fn main() {
     let project_path = Path::new("./");
     let scss_files = find_scss_files(project_path);
-    for file in scss_files {
-        println!("SASS CODE: {}", file);
-    }
 
-    let scss_code = "
-        $main-color: #000000; 
-        body {
-            color: $main-color;
+    for file_name in scss_files {
+        let scss_code = fs::read_to_string(&file_name).unwrap();
+
+        if !scss_code.contains("$") {
+            return;
         }
-    ";
 
-    let lines: Vec<&str> = scss_code.lines().collect();
-    let mut root_css = format!(":root {{\n");
-    let mut new_scss_lines = Vec::new();
+        let lines: Vec<&str> = scss_code.lines().collect();
+        let mut root_css = format!(":root {{\n");
+        let mut new_scss_lines = Vec::new();
 
-    for line in lines {
-        let line = line.trim().replace("$", "--");
+        for line in lines {
+            let line = line.trim().replace("$", "--");
 
-        if line.starts_with("--") {
-            let formated_line = format!("{}\n", line);
-            root_css += &formated_line;
-        } else {
-            if line.contains("--") {
-                let css_rule = line.split("--").nth(0).unwrap();
-                let css_variable_with_semicollon = line.split("--").nth(1).unwrap();
-                let css_variable = css_variable_with_semicollon.replace(";", "");
-
-                let formated_line = format!("{} var(--{});", css_rule, css_variable);
-                new_scss_lines.push(formated_line);
+            if line.starts_with("--") {
+                let formated_line = format!("{}\n", line);
+                root_css += &formated_line;
             } else {
-                new_scss_lines.push(line);
+                if line.contains("--") {
+                    let css_rule = line.split("--").nth(0).unwrap();
+                    let css_variable_with_semicollon = line.split("--").nth(1).unwrap();
+                    let css_variable = css_variable_with_semicollon.replace(";", "");
+
+                    let formated_line = format!("{} var(--{});", css_rule, css_variable);
+                    new_scss_lines.push(formated_line);
+                } else {
+                    new_scss_lines.push(line);
+                }
             }
         }
-    }
 
-    let new_scss_code = root_css + &new_scss_lines.join("\n");
+        root_css = root_css + "}";
+
+        let new_scss_code = root_css + &new_scss_lines.join("\n");
+        fs::write(file_name, new_scss_code).unwrap();
+    }
 }
