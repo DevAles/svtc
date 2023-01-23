af6feeeef5d028396a3f8a89790d0fbf5c404a49
DevAles <devalesoficial@gmail.com>
2023-01-23 18:47:04 -0300

    Get scss files in a project

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
+use std::fs;
+use std::path::Path;
+
+fn find_scss_files(path: &Path) -> Vec<String> {
+    let mut scss_files = Vec::new();
+
+    if path.is_dir() {
+        for file in fs::read_dir(&path).unwrap() {
+            let file = file.unwrap();
+            let file_path = file.path();
+
+            if file_path.is_dir() {
+                scss_files.extend(find_scss_files(&file_path));
+            } else {
+                if file_path.extension().map(|s| s == "scss").unwrap_or(false) {
+                    scss_files.push(file_path.to_str().unwrap().to_owned())
+                }
+            }
+        }
+    }
+
+    scss_files
+}
+
 fn main() {
-    let sass_code = "
+    let project_path = Path::new("./");
+    let scss_files = find_scss_files(project_path);
+    for file in scss_files {
+        println!("SASS CODE: {}", file);
+    }
+
+    let scss_code = "
         $main-color: #000000; 
         body {
             color: $main-color;
         }
     ";
 
-    let lines: Vec<&str> = sass_code.lines().collect();
+    let lines: Vec<&str> = scss_code.lines().collect();
     let mut root_css = format!(":root {{\n");
-    let mut new_sass_lines = Vec::new();
+    let mut new_scss_lines = Vec::new();
 
     for line in lines {
         let line = line.trim().replace("$", "--");
@@ -23,12 +53,12 @@ fn main() {
                 let css_variable = css_variable_with_semicollon.replace(";", "");
 
                 let formated_line = format!("{} var(--{});", css_rule, css_variable);
-                new_sass_lines.push(formated_line);
+                new_scss_lines.push(formated_line);
             } else {
-                new_sass_lines.push(line);
+                new_scss_lines.push(line);
             }
         }
     }
 
-    let new_sass_code = root_css + &new_sass_lines.join("\n");
+    let new_scss_code = root_css + &new_scss_lines.join("\n");
 }
